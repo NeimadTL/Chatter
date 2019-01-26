8f02fa2ac2a132a1cc900c46511c6bc4bf050190
neimad971 <damientalbot26@gmail.com>
2019-01-26 15:38:08 +0100

    Makes class for client

--- client.rb
+++ client.rb
@@ -1,9 +1,29 @@
 require 'socket'
 
-s = TCPSocket.new 'localhost', 2000
-s.puts "Hey server, I'm trying to connect"
-while line = s.gets # Read lines from socket
-  puts line         # and print them
+class Client
+  def initialize(hostname, port)
+    @hostname = hostname
+    @port = port
+    @tcp_socket = TCPSocket.new(hostname, port)
+  end
+
+  def send_message(message)
+    @tcp_socket.puts(message)
+  end
+
+  def read_message
+    while line = @tcp_socket.gets   # Read lines from socket
+      puts line                     # and print them
+    end
+  end
+
+  def close_connection
+    @tcp_socket.close
+  end
 end
 
-s.close             # close socket when done
+
+client = Client.new('localhost', 2000)
+client.send_message("Hey server, I'm trying to connect")
+client.read_message
+client.close_connection
