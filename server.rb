99681943a0f308482b14a150827c604693eed54e
neimad971 <damientalbot26@gmail.com>
2019-01-29 20:59:07 +0100

    prints the number of clients connected to the server

--- server.rb
+++ server.rb
@@ -4,19 +4,25 @@ class Server
   def initialize(port)
     @port = port
     @server_socket = TCPServer.new(port) # Server bound to port 2000
-    @client = nil
+    @clients = []
   end
 
   def start
     puts 'Server started...'
     loop do
       @client = @server_socket.accept   # Wait for a client to connect
+      @clients.push(@client)
+      puts "clients -> #{self.clients_count}" 
       self.read_message_from_client
       self.send_message_to_client("Hello client !")
       self.read_message_from_client
       self.send_message_to_client("you're welcome")
       @client.close
     end
+  end
+
+  def clients_count
+    @clients.length
   end
 
   def send_message_to_client(message)
